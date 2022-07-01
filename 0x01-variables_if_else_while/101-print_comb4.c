de118cd314e3a608a5bb7e61a77a72e9b7a931d3
Thomas Kankam <kankamthomas6@gmail.com>
2022-07-01 13:50:19 -0700

    Updated files for changes in if else while loop

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,10 +1,4 @@
-/*
- * File: 101-print_comb4.c
- * Auth: Brennan D Baraban
- */
-
 #include <stdio.h>
-
 /**
  * main - Prints all possible combinations of three different digits,
  *        in ascending order, separated by a comma followed by a space.
